7b94e647e8ce3ce9bd5d3ab41baed17abb20e7b6
Cynthia Mattingly <cynthia.mattingly@springlane.de>
2020-11-11 08:54:27 +0100

    add setup.sql

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Setup
 
 ## Database
-The quiz requires a database, please execute the SQL found in setup.sql to set up the table structure.
+The quiz requires a database, please execute the SQL found in setup.sql to set up the table structure. (Change the schema name if wished).
 Add your questions to the 'question' table (with quizId 1) and your answers to the 'answer' table.
 
 ##Config
-Add your database connection info to _basic/config/db.php_
+Add your database connection info to _config/db.php_
 
 ## Installing
-Run 'composer install' while in the folder _basic_
+Run 'composer install' while in the root folder
 
 ## Webserver
-Run 'php yii serve' while in the folder _basic_, now you can access the quiz via localhost. 
+Run 'php yii serve' while in the root folder, now you can access the quiz via localhost. 
 The default URL is http://localhost:8080/ 
---
+++ setup.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE `yii2quiz`;
+
+CREATE TABLE `answer` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `questionId` int unsigned DEFAULT NULL,
+  `text` text,
+  `correct` int DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `question_idx` (`questionId`),
+  CONSTRAINT `question` FOREIGN KEY (`questionId`) REFERENCES `question` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `question` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `quizId` int unsigned DEFAULT NULL,
+  `text` text,
+  PRIMARY KEY (`id`),
+  KEY `quizId_idx` (`quizId`),
+  CONSTRAINT `quizId` FOREIGN KEY (`quizId`) REFERENCES `quiz` (`id`)
+);
+
+CREATE TABLE `quiz` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `score` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `quizId` int unsigned DEFAULT NULL,
+  `score` int unsigned DEFAULT NULL,
+  `sessionId` varchar(45) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `dateFinished` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sessionId_UNIQUE` (`sessionId`),
+  KEY `quizId_idx` (`quizId`),
+  CONSTRAINT `quizToScore` FOREIGN KEY (`quizId`) REFERENCES `quiz` (`id`)
+);
+
+INSERT INTO `quiz`
+(`id`, `name`)
+VALUES
+(1, 'Default Quiz');
